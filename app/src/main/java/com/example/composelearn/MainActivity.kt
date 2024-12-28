a6676071c0cd8af182f67a5437e9656757b20e2b
danil555565 <danil555565@mail.ru>
2024-12-28 23:01:18 +0300

    2

--- app/src/main/java/com/example/composelearn/MainActivity.kt
+++ app/src/main/java/com/example/composelearn/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composelearn.ui.theme.ComposeLearnTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +35,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeLearnTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {
+                    Quadrant()
+//                    TaskComplete(modifier = Modifier)
+//                    Article( modifier = Modifier.padding(8.dp))
+//                    GreetingImage(
+//                        message = "Happy Birthday Sam!",
+//                        from = "From Emma",
+//                        modifier = Modifier.padding(8.dp)
+//                    )
                 }
             }
         }
@@ -31,17 +54,165 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun Quadrant() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            Card(
+                title = "stringResource(R.string.first_title)",
+                description = "stringResource(R.string.first_description)",
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            Card(
+                title = "stringResource(R.string.first_title)",
+                description = "stringResource(R.string.first_description)",
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(Modifier.weight(1f)) {
+            Card(
+                title = "stringResource(R.string.first_title)",
+                description = "stringResource(R.string.first_description)",
+                backgroundColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            Card(
+                title = "stringResource(R.string.first_title)",
+                description = "stringResource(R.string.first_description)",
+                backgroundColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+
+    }
+}
+
+@Composable
+private fun Card(title: String, description: String, modifier: Modifier, backgroundColor: Color) {
+    Box() {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .background(backgroundColor)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = title)
+            Text(text = description)
+        }
+    }
+}
+
+
+@Composable
+fun TaskComplete(modifier: Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center, modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_task_completed),
+            contentDescription = null,
+            modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = stringResource(R.string.completed),
+            fontSize = 24.sp,
+            modifier = modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = stringResource(R.string.gg),
+            fontSize = 24.sp,
+            modifier = modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+
+    }
+}
+
+@Composable
+fun Article(
+    modifier: Modifier = Modifier
+
+) {
+    Column {
+        Image(
+            painter = painterResource(R.drawable.bg_compose_background), contentDescription = null,
+//            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        Text(
+            text = stringResource(R.string.title),
+            fontSize = 24.sp,
+            modifier = modifier.padding(16.dp)
+        )
+        Text(
+            text = stringResource(R.string.article), modifier.padding(16.dp)
+        )
+        Text(
+            text = stringResource(R.string.description), modifier.padding(16.dp)
+        )
+
+    }
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello $name!", modifier = modifier
     )
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Box(Modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        GreetingText(
+            message = message, from = from, modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+}
+
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center, modifier = modifier
+    ) {
+        Text(
+            text = stringResource(R.string.title),
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+                .background(color = Color.Green)
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeLearnTheme {
-        Greeting("Android")
+        GreetingImage("Android", "google")
     }
 }
